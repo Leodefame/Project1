4a8c363454b088b58df378c5b017ce4a381d63c1
[omitted] <[omitted]>
2017-02-06 22:06:32 -0500

    Divide Function

--- Factorial.java
+++ Factorial.java
@@ -6,10 +6,4 @@ public class Factorial
 			System.out.println( i + "! is " + factorial(i));
 	}
 	
-	public static int factorial(int n)
-	{	int result = 1;
-		for(int i = 2; i <= n; i++)
-			result *= i;
-		return result;
-	}
 }
